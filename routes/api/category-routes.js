44f34586eb884c0b0d65aa8f49775ac4df110aed
Farhod Akramov <akramovfrank@gmail.com>
2023-04-21 21:00:26 -0600

    finished routes for categoryRoutes.js time to move on to productRoutes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,16 +31,53 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
-  // create a new category
+router.post("/", async (req, res) => {
+  try {
+    const categoryData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
-router.put("/:id", (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "Category with that ID doesn't exist" });
+      return;
+    } else {
+      res.status(200).json(categoryData);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.delete("/:id", (req, res) => {
-  // delete a category by its `id` value
+router.delete("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "Category with that ID doesn't exist" });
+      return;
+    } else {
+      res.status(200).json(categoryData);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
